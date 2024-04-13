ca6d3d41313950417af95a1509169e37cb3bf510
naskovik <nazarskoreyko@gmail.com>
2024-04-13 19:51:34 +0200

    fix: add catppuccin

--- lua/nazar/lazy/colors.lua
+++ lua/nazar/lazy/colors.lua
@@ -1,6 +1,6 @@
 function ColorDeezPencils(color)
 
-    color = color or "nordic"
+    color = color or "catppuccin"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -44,12 +44,48 @@ return {
     },
 
     {
-        'AlexvZyl/nordic.nvim',
-        lazy = false,
+        "catppuccin/nvim",
+        name = "catppuccin",
         priority = 1000,
         config = function()
-            require 'nordic' .load()
-            ColorDeezPencils()
+            require('catppuccin').setup({
+                flavour = "macchiato", -- latte, frappe, macchiato, mocha
+                background = { -- :h background
+                    light = "latte",
+                    dark = "macchiato",
+                },
+                transparent_background = false, -- disables setting the background color.
+                show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+                term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+                dim_inactive = {
+                    enabled = false, -- dims the background color of inactive window
+                    shade = "dark",
+                    percentage = 0.15, -- percentage of the shade to apply to the inactive window
+                },
+                no_italic = true, -- Force no italic
+                no_bold = true, -- Force no bold
+                no_underline = false, -- Force no underline
+                styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+                    comments = { nil }, -- Change the style of comments
+                    conditionals = { nil },
+                },
+                color_overrides = {},
+                custom_highlights = {},
+                default_integrations = true,
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    nvimtree = true,
+                    treesitter = true,
+                    notify = false,
+                    mini = {
+                        enabled = true,
+                        indentscope_color = "",
+                    },
+                    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+                },
+            })
+            ColorDeezPencils('catppuccin')
         end
     }
 
