af47d4e930fa8b195490260e969452efcc50acb3
naskovik <nazarskoreyko@gmail.com>
2024-04-29 18:59:13 +0200

    nvim-grey

--- lua/nazar/lazy/colors.lua
+++ lua/nazar/lazy/colors.lua
@@ -11,73 +11,54 @@ end
 return {
 
     {
-        "rebelot/kanagawa.nvim",
-        config = function()
-            require("kanagawa").setup({
-                commentStyle = { italic = false },
-                keywordStyle = { italic = false },
-                statementStyle = { bold = true },
-                theme = "wave",              -- Load "wave" theme when 'background' option is not set
-                background = {               -- map the value of 'background' option to a theme
-                    dark = "wave",           -- try "dragon" !
-                    light = "lotus"
-                },
-            })
-
-        end
-
-    },
-
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require('catppuccin').setup({
-                flavour = "macchiato", -- latte, frappe, macchiato, mocha
-                background = { -- :h background
-                    light = "latte",
-                    dark = "macchiato",
-                },
-                transparent_background = false, -- disables setting the background color.
-                show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-                term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
-                dim_inactive = {
-                    enabled = false, -- dims the background color of inactive window
-                    shade = "dark",
-                    percentage = 0.15, -- percentage of the shade to apply to the inactive window
-                },
-                no_italic = true, -- Force no italic
-                no_bold = true, -- Force no bold
-                no_underline = false, -- Force no underline
-                styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-                    comments = { nil }, -- Change the style of comments
-                    conditionals = { nil },
-                },
-                color_overrides = {},
-                custom_highlights = {},
-                default_integrations = true,
-                integrations = {
-                    cmp = true,
-                    gitsigns = true,
-                    nvimtree = true,
-                    treesitter = true,
-                    notify = false,
-                    mini = {
-                        enabled = true,
-                        indentscope_color = "",
-                    },
-                    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-                },
-            })
-
-        end
-    },
-
-
-    {
         "tinted-theming/base16-vim",
         config = function() ColorDeezPencils() end
-    }
+    },
+
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function ()
+            require("tokyonight").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                light_style = "day", -- The theme is used when the background is set to light
+                transparent = false, -- Enable this to disable setting the background color
+                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+                styles = {
+                    -- Style to be applied to different syntax groups
+                    -- Value is any valid attr-list value for `:help nvim_set_hl`
+                    comments = { italic = true },
+                    keywords = { italic = false },
+                    functions = {},
+                    variables = {},
+                    -- Background styles. Can be "dark", "transparent" or "normal"
+                    sidebars = "dark", -- style for sidebars, see below
+                    floats = "dark", -- style for floating windows
+                },
+                sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+                day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+                hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+                dim_inactive = false, -- dims inactive windows
+                lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+                --- You can override specific color groups to use other groups or a hex color
+                --- function will be called with a ColorScheme table
+                ---@param colors ColorScheme
+                on_colors = function(colors) end,
+
+                --- You can override specific highlights to use other groups or a hex color
+                --- function will be called with a Highlights and ColorScheme table
+                ---@param highlights Highlights
+                ---@param colors ColorScheme
+                on_highlights = function(highlights, colors) end,
+            })
+        end
+    },
+
+    { 'yorickpeterse/nvim-grey' }
 
 }
