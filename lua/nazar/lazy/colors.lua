7dace737f7402651a99b74cfcb571d5a8945b711
naskovik <nazarskoreyko@gmail.com>
2024-05-05 21:51:10 +0200

    keep background

--- lua/nazar/lazy/colors.lua
+++ lua/nazar/lazy/colors.lua
@@ -3,12 +3,42 @@ function ColorDeezPencils(color)
     color = color or "base16-gruvbox-dark-medium"
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 end
 
 return {
+
+    {
+        "rebelot/kanagawa.nvim",
+        config = function()
+            require('kanagawa').setup({
+                compile = false,             -- enable compiling the colorscheme
+                undercurl = true,            -- enable undercurls
+                commentStyle = { italic = false },
+                functionStyle = {},
+                keywordStyle = { italic = false },
+                statementStyle = { bold = false },
+                typeStyle = {},
+                transparent = false,         -- do not set background color
+                dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+                terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+                colors = {                   -- add/modify theme and palette colors
+                    palette = {},
+                    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+                },
+                overrides = function(colors) -- add/modify highlights
+                    return {}
+                end,
+                theme = "wave",              -- Load "wave" theme when 'background' option is not set
+                background = {               -- map the value of 'background' option to a theme
+                    dark = "wave",           -- try "dragon" !
+                    light = "lotus"
+                },
+            })
+        end
+    },
 
     {
         "tinted-theming/base16-vim",
