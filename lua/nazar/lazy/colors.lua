61ed587007c6f061cc53cfc5d17a3749a4fba9c4
naskovik <nazarskoreyko@gmail.com>
2024-05-20 22:44:02 +0200

    kanagawa

--- lua/nazar/lazy/colors.lua
+++ lua/nazar/lazy/colors.lua
@@ -1,6 +1,6 @@
-function ColorDeezPencils(color)
+function ApplyColors(color)
 
-    color = color or "base16-catppuccin-macchiato"
+    color = color or "kanagawa"
     vim.cmd.colorscheme(color)
 
 --    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -10,19 +10,116 @@ end
 
 return {
 
-
     {
-        "tinted-theming/base16-vim",
+        "rose-pine/neovim",
+        name = "rose-pine",
         config = function()
-            ColorDeezPencils()
+            require("rose-pine").setup({
+                variant = "moon", -- auto, main, moon, or dawn
+                dark_variant = "main", -- main, moon, or dawn
+                dim_inactive_windows = false,
+                extend_background_behind_borders = true,
+
+                enable = {
+                    terminal = true,
+                    legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+                    migrations = true, -- Handle deprecated options automatically
+                },
+
+                styles = {
+                    bold = false,
+                    italic = false,
+                    transparency = true,
+                },
+
+                groups = {
+                    border = "muted",
+                    link = "iris",
+                    panel = "surface",
+
+                    error = "love",
+                    hint = "iris",
+                    info = "foam",
+                    note = "pine",
+                    todo = "rose",
+                    warn = "gold",
+
+                    git_add = "foam",
+                    git_change = "rose",
+                    git_delete = "love",
+                    git_dirty = "rose",
+                    git_ignore = "muted",
+                    git_merge = "iris",
+                    git_rename = "pine",
+                    git_stage = "iris",
+                    git_text = "rose",
+                    git_untracked = "subtle",
+
+                    h1 = "iris",
+                    h2 = "foam",
+                    h3 = "rose",
+                    h4 = "gold",
+                    h5 = "pine",
+                    h6 = "foam",
+                },
+
+                highlight_groups = {
+                    -- Comment = { fg = "foam" },
+                    -- VertSplit = { fg = "muted", bg = "muted" },
+                },
+
+                before_highlight = function(group, highlight, palette)
+                    -- Disable all undercurls
+                    -- if highlight.undercurl then
+                    --     highlight.undercurl = false
+                    -- end
+                    --
+                    -- Change palette colour
+                    -- if highlight.fg == palette.pine then
+                    --     highlight.fg = palette.foam
+                    -- end
+                end,
+            })
+
         end
     },
 
+    {
+        "rebelot/kanagawa.nvim",
+        config = function()
+            require('kanagawa').setup({
+                compile = false,             -- enable compiling the colorscheme
+                undercurl = true,            -- enable undercurls
+                commentStyle = { italic = true },
+                functionStyle = {},
+                keywordStyle = { italic = false},
+                statementStyle = { bold = false },
+                typeStyle = {},
+                transparent = false,         -- do not set background color
+                dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+                terminalColors = false,       -- define vim.g.terminal_color_{0,17}
+                colors = {                   -- add/modify theme and palette colors
+                    palette = {},
+                    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+                },
+                overrides = function(colors) -- add/modify highlights
+                    return {}
+                end,
+                theme = "wave",              -- Load "wave" theme when 'background' option is not set
+                background = {               -- map the value of 'background' option to a theme
+                    dark = "wave",           -- try "dragon" !
+                    light = "lotus"
+                },
+            })
+
+            ApplyColors("kanagawa")
+
+        end
+    },
 
     {
         "scottmckendry/cyberdream.nvim",
         lazy = false,
-        priority = 1000,
         config = function()
             require("cyberdream").setup({
                 -- Recommended - see "Configuring" below for more config options
