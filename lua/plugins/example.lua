7511f6d892554ca91e16be3d934bfcaffe3188af
naskovik <nazarskoreyko@gmail.com>
2024-11-15 14:40:58 +0100

    lazyvim distro conf

---
+++ lua/plugins/example.lua
@@ -0,0 +1,24 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  -- add gruvbox
+  --{ "ellisonleao/gruvbox.nvim" },
+  -- add kanagawa
+  { 'rebelot/kanagawa.nvim' },
+  { 'AlexvZyl/nordic.nvim' },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nordic",
+    },
+  },
+
+}
